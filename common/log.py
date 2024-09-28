91a127a050780ca96a0a65c7d47fb52f6315dba6
akun.vmware.win11 <akun@vmware.win11>
2024-09-28 18:17:18 +0800

    m

--- common/log.py
+++ common/log.py
@@ -1,34 +1,48 @@
+import os
 from loguru import logger
 import sys
-import logging
 
-# 配置日志记录
+# 确保 Python 环境使用 UTF-8 编码
+os.environ['PYTHONIOENCODING'] = 'utf-8'
+
+# 配置日志记录器
 logger.remove()  # 移除默认的日志记录器
 
-# 添加控制台日志记录器
-logger.add(sys.stdout, format="[{level}][{time:HH:mm:ss}]-{message} [{file}:{line}]")
+# 添加控制台日志记录器，设置为 info 级别
+logger.add(
+    sys.stdout,
+    format="[{level}][{time:HH:mm:ss}]-{message} [{file}:{line}]",
+    level="INFO",
+    colorize=True  # 控制台日志彩色输出
+)
 
-# 添加文件日志记录器，按日期命名
-logger.add("logs/run_{time:YYYY-MM-DD}.log", format="[{level}][{time:YYYY-MM-DD HH:mm:ss}] - {message} [{file}:{line}]", rotation="00:00", encoding="utf-8")
+# 添加文件日志记录器，按日期命名，设置为 info 级别
+logger.add(
+    "logs/run_{time:YYYY-MM-DD}.log",
+    format="[{level}][{time:YYYY-MM-DD HH:mm:ss}] - {message} [{file}:{line}]",
+    rotation="1 day",  # 每天午夜轮换日志文件
+    encoding="utf-8",
+    level="INFO"
+)
 
-# 添加错误日志记录器，按日期命名
-logger.add("logs/err_{time:YYYY-MM-DD}.log", level="ERROR", format="[{level}][{time:YYYY-MM-DD HH:mm:ss}] - {message} [{file}:{line}]", rotation="00:00", encoding="utf-8")
+# 添加错误日志记录器，按日期命名，设置为 error 级别
+logger.add(
+    "logs/err_{time:YYYY-MM-DD}.log",
+    level="ERROR",
+    format="[{level}][{time:YYYY-MM-DD HH:mm:ss}] - {message} [{file}:{line}]",
+    rotation="1 day",  # 每天午夜轮换日志文件
+    encoding="utf-8"
+)
 
-# 兼容 Python 自带的 logging 模块的输入参数
-class InterceptHandler(logging.Handler):
-    def emit(self, record):
-        # 获取 loguru 的 logger
-        loguru_logger = logger.opt(depth=6, exception=record.exc_info)
-        loguru_logger.log(record.levelname, record.getMessage())
+# 为警告级别的日志添加别名
+def warn(*args, **kwargs):
+    logger.warning(*args, **kwargs)
 
-logging.basicConfig(handlers=[InterceptHandler()], level=0)
+# 将 warn 函数添加到 logger 对象中
+logger.warn = warn
 
 # 示例日志记录
 logger.info("这是一个信息日志")
 logger.error("这是一个错误日志")
-
-# 正确的用法
 logger.warning('This is a warning message')
-# 添加 warn 方法作为 warning 方法的别名
-logger.warn = logger.warning
-logger.warn('This is a warning message2')
+logger.warn('This is another warning message')  # 使用别名记录警告日志
