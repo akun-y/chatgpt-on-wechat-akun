51186a75a3b9b53eecbe8f833813da9698708570
akun2024 <akun2024@win11.com>
2024-12-27 11:06:26 +0800

    fix: log level

--- common/log.py
+++ common/log.py
@@ -8,8 +8,6 @@ from config import conf
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
 
-
-
 # 为警告级别的日志添加别名
 def warn(*args, **kwargs):
     logger.warning(*args, **kwargs)
@@ -22,12 +20,12 @@ logger.warn = warn
 # logger.info("这是一个信息日志")
 # logger.error("这是一个错误日志")
 # logger.warning('This is a warning message')
-# logger.warn('This is another warning message')  # 使用别名记录警告日志
+# logger.warn('This is another warning message')  # 使用别名记录告日志
 
 logger = logger
 
 def set_logger():
-    level = "DEBUG" if conf().get("debug") else "INFO"
+    level = "DEBUG" if conf().get("debug") == True else "INFO"
     print(f"set_logger level: {level}")
     # 配置日志记录器
     logger.remove()  # 移除默认的日志记录器
@@ -57,8 +55,8 @@ def set_logger():
         encoding="utf-8",
     )
 
+
 def _reset_logger(logger_instance):
     """重置logger配置"""
     set_logger()
     return logger_instance
-
